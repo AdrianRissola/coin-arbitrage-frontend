f7590daf92c2bb1fd6fd2c51967aec3fdf2628ee
Adrián R <adryanpr@gmail.com>
2022-08-30 19:54:59 -0300

    .env files

--- src/params.js
+++ src/params.js
@@ -1,9 +1,9 @@
-
-
 export const getWebsocketEndpoint = () => {
     let websocketEndpoint = process.env.REACT_APP_PROD_WEB_SOCKET_ENDPOINT
     if(process.env.NODE_ENV!=="production")
         websocketEndpoint = process.env.REACT_APP_LOCAL_WEB_SOCKET_ENDPOINT
+    else
+        websocketEndpoint = "wss://arcane-refuge-64485.herokuapp.com/"
     console.log("websocketEndpoint: ", websocketEndpoint)
-    return websocketEndpoint
+    return "wss://arcane-refuge-64485.herokuapp.com/"
 }
