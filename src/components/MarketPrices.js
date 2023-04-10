ed123825678163f3d944cfed84cb59efef7cdd93
Adrián R <adryanpr@gmail.com>
2023-04-10 12:20:02 -0300

    show empty table/card when there is no prices for selectet currency pair

--- src/components/MarketPrices.js
+++ src/components/MarketPrices.js
@@ -13,41 +13,44 @@ const MarketPrices = (props)=> {
     return(
         <div className={styles.cardClassName} style={styles.cardStyle}>
             <div className={styles.cardBodyClassName}>
-                <table className="table" style={{ color: textColor}}>
-                    <thead>
-                        <tr>
-                            <th className="text-center" style={{fontSize:"18px"}}>Prices</th>
-                            <th>
-                                <div className="form-check">
-                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"
-                                        onClick={ evt => {setOrder(evt.target.checked)} }
-                                    />
-                                    <label style={{fontSize:"18px"}}>
-                                        &#x2191;&#x2193;
-                                    </label>
-                                </div>
-                            </th>
-                        </tr>
-                        <tr>
-                            <th>{ticker.split('-')[0]} Markets ({props.marketPrices.length})</th>
-                            <th>Price ({ticker.split('-')[1]})</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            marketPrices.map(marketPrice => {
-                                return (
-                                    <tr key={marketPrice.market}>
-                                        <td style={{ textAlign:"left", fontWeight: 'bold' }}>{ marketPrice.market }</td>
-                                        <td style={{ color:greenHex, textAlign:"right", fontWeight: 'bold' }} title={marketPrice.price}>
-                                            { marketPrice.price.toLocaleString(navigator.language, {maximumSignificantDigits:7}) }
-                                        </td>
-                                    </tr>
-                                );
-                            })
-                        }
-                    </tbody>
-                </table>
+                { props.marketPrices ?
+                    <table className="table" style={{ color: textColor}}>
+                        <thead>
+                            <tr>
+                                <th className="text-center" style={{fontSize:"18px"}}>Prices</th>
+                                <th>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"
+                                            onClick={ evt => {setOrder(evt.target.checked)} }
+                                        />
+                                        <label style={{fontSize:"18px"}}>
+                                            &#x2191;&#x2193;
+                                        </label>
+                                    </div>
+                                </th>
+                            </tr>
+                            <tr>
+                                <th>{ticker.split('-')[0]} Markets ({props.marketPrices.length})</th>
+                                <th>Price ({ticker.split('-')[1]})</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                marketPrices.map(marketPrice => {
+                                    return (
+                                        <tr key={marketPrice.market}>
+                                            <td style={{ textAlign:"left", fontWeight: 'bold' }}>{ marketPrice.market }</td>
+                                            <td style={{ color:greenHex, textAlign:"right", fontWeight: 'bold' }} title={marketPrice.price}>
+                                                { marketPrice.price.toLocaleString(navigator.language, {maximumSignificantDigits:7}) }
+                                            </td>
+                                        </tr>
+                                    );
+                                })
+                            }
+                        </tbody>
+                    </table>
+                    : null
+                }
             </div>
         </div>      
     )
