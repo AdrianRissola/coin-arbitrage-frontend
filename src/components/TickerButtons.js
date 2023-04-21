65ca786ac1411e12f6e11d643fcad15c989ac5fe
Adrián R <adryanpr@gmail.com>
2023-04-21 16:47:40 -0300

    refactor style

--- src/components/TickerButtons.js
+++ src/components/TickerButtons.js
@@ -1,17 +1,35 @@
+import React from "react";
+
 const TickerButtons = (props) => {
     const tickers = props.tickers.sort((t1, t2) => (t1.name > t2.name) ? 1 : ((t2.name > t1.name) ? -1 : 0))
     const handleChangeChannelSubscriptionClick = props.handleChangeChannelSubscriptionClick
     const btnClassName = props.darkMode ? "btn btn-dark" : "btn btn-light"
-    const backgroundColor = !props.darkMode ? "#E9ECEF" : null
     
+    // return tickers.map(ticker => {
+    //   return (
+    //     <button className={btnClassName} key={ticker.name}
+    //       title={ticker.description}
+    //       style={{whiteSpace:'nowrap', height: '37.6px', marginRight: '0.3rem', marginTop: '0.3rem',  fontSize:'90%', backgroundColor:backgroundColor}} 
+    //       onClick={ ()=>{ handleChangeChannelSubscriptionClick({channel: 'arbitrage', ticker: ticker.name}) } }>
+    //       {ticker.name}
+    //     </button>
+    //   )
+    // })
+
+
     return tickers.map(ticker => {
       return (
-        <button className={btnClassName} key={ticker.name}
-          title={ticker.description}
-          style={{whiteSpace:'nowrap', height: '37.6px', marginRight: '0.3rem', marginTop: '0.3rem',  fontSize:'90%', backgroundColor:backgroundColor}} 
-          onClick={ ()=>{ handleChangeChannelSubscriptionClick({channel: 'arbitrage', ticker: ticker.name}) } }>
-          {ticker.name}
-        </button>
+        <React.Fragment key={ ticker.name }>
+          <input onClick={ ()=>{ handleChangeChannelSubscriptionClick({channel: 'arbitrage', ticker: ticker.name}) } } 
+            type="radio" className="btn-check" name="btnradioTciker" defaultChecked={ticker.name==='BTC-USDT' ? true : false}
+            id={ticker.name} autoComplete="off"
+          />
+          <label className={btnClassName} htmlFor={ticker.name}
+            style={{whiteSpace:'nowrap', marginRight: '0.3rem', marginTop: '0.3rem'}} 
+          >
+            {ticker.name}
+          </label>
+        </React.Fragment>
       )
     })
 }
