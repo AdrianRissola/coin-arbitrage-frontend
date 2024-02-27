0adaeba06ea99ee9369624c160a11a34a9df54da
Adrián R <adryanpr@gmail.com>
2024-02-27 00:22:17 -0300

    refactor search and filters layout

--- src/components/views/HistoricalView.js
+++ src/components/views/HistoricalView.js
@@ -2,7 +2,6 @@ import React, { useState, useCallback } from "react";
 import { getHistoricalArbitrages } from "../../service/HistoricalArbitrageService"
 import ArbitrageList from "../ArbitrageList";
 import helper from "../../helper";
-import RowFilter from "../../components/filters/RowFilter";
 import SplitButtonComboBox from "../../components/filters/SplitButtonComboBox";
 import MarketFilter from "../../components/filters/MarketFilter";
 import MinProfitFilter from "../../components/filters/MinProfitFilter";
@@ -16,6 +15,7 @@ const HistoricalView = (props)=> {
     const [marketFilter, setMarketFilter] = useState("");
     const [marketsFilter, setMarketsFilter] = useState([]);
     const [minProfitFilter, setMinProfitFilter] = useState(0)
+    const [showFilters, setShowFilters] = useState(false)
     const [historicalArbitrageOrder, setHistoricalArbitrageOrder] = useState({key: "date", value: "DESC", label: "Date"})
     const availableTickers = [...JSON.parse(localStorage.getItem("availableWebsocketTickers"))];
     const darkMode = props.darkMode;
@@ -52,49 +52,97 @@ const HistoricalView = (props)=> {
         setHistoricalArbitrageOrder(selectedHistoricalArbitrageOrder)  
     }
 
-    const getArbitrageFilters = () => {
+    const buildInputGroup = (component, width, marginRight) => {
         return (
-          [
-            <MarketFilter marketFilter = {marketFilter} marketFilterSetFunction = {setMarketFilter} darkMode={darkMode} 
-                marginRight={"0.1rem"} styleWidth = {"210px"}
-            />,
-            <MinProfitFilter minProfitFilter = {minProfitFilter} minProfitFilterSetFunction = {setMinProfitFilter} 
-                darkMode={darkMode} marginRight={"0.1rem"} styleWidth = {"210px"}
-            />,
-          ]
+            <div className="input-group mb-3"
+                style={{ 
+                    display:"flex",
+                    width: width || "fit-content",
+                    marginRight: marginRight,
+                    marginLeft: '0px' 
+                }}
+            >
+                {component}
+            </div>
         )
     }
-    
-    const getHistoricalFilters = () => {
-        const historicalFilters = getArbitrageFilters();
-        historicalFilters.push(
-            <MarketsCheckboxDropdownFilter darkMode = { darkMode } onClickFunction = { setMarketsFilter }
-                marketsFilter = { marketsFilter } buttonText = { "Markets"} styleWidth = {"110px"} marginRight={"0.1rem"}
-            />
-        );
-        historicalFilters.push(
-            <TickersCheckboxDropdownFilter darkMode = { darkMode }
-                handleOnClickFindButton = { handleOnClickFindButton }
-                buttonText = { "Tickers"} styleWidth = {"150px"} marginRight={"0.1rem"}
-            />
-        );
-        
-        historicalFilters.push(
-        <SplitButtonComboBox darkMode = { darkMode } 
-            optionOnClickFunction = { handleSelectedHistoricalArbitrageOrderOnClick }
-            buttonOnClickFunction = { handleSelectedHistoricalArbitrageOrderOnClickk }
-            currentSelection = { historicalArbitrageOrder } buttonText = { "Order By: "}
-            options = { 
-                [{key: "profitPercentage", value: "DESC", label: "Profit %"}, {key: "date", value: "DESC", label: "Date"}]
-            }
-            styleWidth = {"220px"}
-        />);
 
-        return historicalFilters;
-    }
-
-    const getHistoricalRowFilterComponent = () => {
-        return ( <RowFilter filters = { getHistoricalFilters() }/> )
+    const getSearchAndFilters = () => {
+        return ( 
+            <div className="row" style={{display:"flex", flexDirection:"row", marginLeft:"50px"}}>
+                <div className="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-3" style={{display:"flex", flexDirection:"row", justifyContent:"left"}}>
+                    {
+                        buildInputGroup(
+                            <TickersCheckboxDropdownFilter darkMode = { darkMode }
+                                handleOnClickFindButton = { handleOnClickFindButton }
+                                buttonText = { "Tickers "} styleWidth = {"200px"}
+                            />, 
+                            "auto"
+                        )
+                    }
+                    <div className="input-group-prepend">
+                        <button type="button" className={darkMode ? "btn text-white bg-dark" : "btn text-white bg-dark"} 
+                            style={{height:'fit-content', marginLeft:'1rem'}} 
+                            onClick={()=>{setShowFilters(prev=>!prev)}}>
+                            Refine
+                            { 
+                                !showFilters ?
+                                <svg style={{marginLeft: "10px", marginBottom: "3px"}}
+                                    xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+                                    <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"></path>
+                                </svg>
+                                :
+                                <svg style={{marginLeft: "10px", marginBottom: "3px"}}
+                                    xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"></path>
+                                </svg>
+                            }
+                        </button>
+                    </div>
+                </div>
+                <div className="col-12 col-sm-12 col-md-12 col-lg-9 col-xl-9" hidden={!showFilters}
+                    style={{display:"flex", flexDirection:"row", flexWrap:"wrap", justifyContent:"left"}}>
+                    {
+                        buildInputGroup(
+                            <MarketFilter marketFilter = {marketFilter} marketFilterSetFunction = {setMarketFilter} darkMode={darkMode} 
+                                styleWidth = {"100px"}
+                            />,
+                            "200px", '1rem'
+                        )
+                    }
+                    {
+                        buildInputGroup(
+                            <MinProfitFilter minProfitFilter = {minProfitFilter} minProfitFilterSetFunction = {setMinProfitFilter} 
+                                darkMode={darkMode}
+                            />,
+                            "200px", '1rem'
+                        )
+                    }
+                    {
+                        buildInputGroup(
+                            <MarketsCheckboxDropdownFilter darkMode = { darkMode } onClickFunction = { setMarketsFilter }
+                                marketsFilter = { marketsFilter } buttonText = { "Markets"} styleWidth = {"100px"} marginRight={"0.1rem"}
+                            />,
+                            "auto", '1rem'
+                        )
+                    }
+                    {
+                        buildInputGroup(
+                            <SplitButtonComboBox darkMode = { darkMode } 
+                                optionOnClickFunction = { handleSelectedHistoricalArbitrageOrderOnClick }
+                                buttonOnClickFunction = { handleSelectedHistoricalArbitrageOrderOnClickk }
+                                currentSelection = { historicalArbitrageOrder } buttonText = { "Order By: "}
+                                options = { 
+                                    [{key: "profitPercentage", value: "DESC", label: "Profit %"}, {key: "date", value: "DESC", label: "Date"}]
+                                }
+                                styleWidth = {"220px"}
+                            />,
+                            "auto", '1rem'
+                        )
+                    }
+                </div>
+            </div>
+        )
     }
 
     return(
@@ -106,7 +154,7 @@ const HistoricalView = (props)=> {
                     </h1>
                 </div>
             </div>
-            { getHistoricalRowFilterComponent() }
+            { getSearchAndFilters() }
             {
                 !historicalArbitrages ?
                 <div className="row justify-content-center">
