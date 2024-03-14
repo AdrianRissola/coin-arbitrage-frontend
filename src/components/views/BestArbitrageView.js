820d0e304636a152f41bdaed487f6d55cfcf3e62
Adrián R <adryanpr@gmail.com>
2024-03-14 18:47:28 -0300

    refactor best arbitrage view allowing multiple coins

--- src/components/views/BestArbitrageView.js
+++ src/components/views/BestArbitrageView.js
@@ -1,5 +1,5 @@
 import { Tooltip } from "@mui/material";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import Arbitrage from "../Arbitrage";
 import MarketPrices from "../MarketPrices";
 
@@ -8,12 +8,10 @@ const BestArbitrageView = (props)=> {
     const darkMode = props.darkMode;
     const bestArbitrages = props.bestArbitrage;
     const marketPrices = props.marketPrices;
-    
     const selectedArbitrage =
-    coin && bestArbitrages ?
-    bestArbitrages.filter(arbitrage => arbitrage.transactions[0].pair.split('-')[1]===coin)[0]
-    : bestArbitrages[0].profitPercentage > bestArbitrages[1].profitPercentage ? 
-        bestArbitrages[0] : bestArbitrages[1];
+    (coin && bestArbitrages) 
+        ? bestArbitrages.filter(arbitrage => arbitrage.transactions[0].pair.split('-')[1]===coin)[0]
+        : bestArbitrages.reduce((bestArbit1, bestArbit2) => bestArbit1.profitPercentage > bestArbit2.profitPercentage ? bestArbit1 : bestArbit2)
     
     const selectedMarketPrices = marketPrices[selectedArbitrage.transactions[0].pair]
 
@@ -21,21 +19,32 @@ const BestArbitrageView = (props)=> {
 
     return(
         <div style={{marginTop:"10px"}}>
-            <div className="row" style={{display: "flex", flexDirection: "col", flexWrap: "wrap", justifyContent: "center" }}>
-                <div className="col-4" style={{display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent: "center"}}>
-                    <div className="btn-group" role="group" >
-                        <input onClick={ () => {setCoin()} } defaultChecked={true} type="radio" className="btn-check" name="btnradio" id="btnradio1" autoComplete="off"/>
-                        <Tooltip title="Best Arbitrage" arrow enterTouchDelay={0} >
-                            <label className={btnGroupClassName} htmlFor="btnradio1">Best</label>
-                        </Tooltip>
-                        <input onClick={ () => {setCoin('USDT')} } type="radio" className="btn-check" name="btnradio" id="btnradio2" autoComplete="off"/>
-                        <Tooltip title="Best USDT Arbitrage" arrow enterTouchDelay={0} >
-                            <label className={btnGroupClassName} htmlFor="btnradio2">USDT</label>
-                        </Tooltip>
-                        <input onClick={ () => {setCoin('BTC')} } type="radio" className="btn-check" name="btnradio" id="btnradio3" autoComplete="off"/>
-                        <Tooltip title="Best BTC Arbitrage" arrow enterTouchDelay={0} >
-                            <label className={btnGroupClassName} htmlFor="btnradio3">BTC</label>
-                        </Tooltip>
+            <div className="row justify-content-center">
+                <div className="col-8">
+                    <div className="row" style={{display: "flex", flexDirection: "col", flexWrap: "wrap", justifyContent: "center" }}>
+                        <div className="btn-group position-relative overflow-auto" role="group" >
+                            <input onClick={ () => {setCoin()} } defaultChecked={true} type="radio" className="btn-check" name="btnradio" id="btnradio1" autoComplete="off"/>
+                            <Tooltip title="Best Arbitrage" arrow enterTouchDelay={0} >
+                                <label className={btnGroupClassName} htmlFor="btnradio1">Best</label>
+                            </Tooltip>
+                            { 
+                                bestArbitrages.map(bestArbit => bestArbit.transactions[0].pair.split("-")[1]).sort().map(
+                                    quote => {
+                                        return(
+                                            <Fragment>
+                                                <input onClick={ () => { setCoin(quote) } } type="radio" 
+                                                className="btn-check" name="btnradio" id={quote} autoComplete="off"/>
+                                                <Tooltip title="Best Arbitrage" arrow enterTouchDelay={0} >
+                                                    <label className={btnGroupClassName} htmlFor={quote}>
+                                                        {quote}
+                                                    </label>
+                                                </Tooltip>
+                                            </Fragment>
+                                        )
+                                    }
+                                )
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
